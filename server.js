e5f6d5c941ecef2cbc0a0b0d864d58cd26bf75da
Shailesh2003-arch <117181669+Shailesh2003-arch@users.noreply.github.com>
2025-01-17 21:04:10 +0530

    done-with-route-parameters

--- server.js
+++ server.js
@@ -64,6 +64,40 @@ app.post("/Movies", (req, res) => {
 //   });
 // });
 
+// here if we want to create a dedicated response for a route parameter, then we do following.
+
+app.get("/Movies/:id", (req, res) => {
+  //we mention the required route parameter using the : parameterName, we can specify any number of paramteres to the route parameter list by using : and as well as we can make any route parameter to be optional by using ?  right in front of the route paramter...
+
+  // Now how to know what value has been passed to the route parameter ?. we can do it by using the 'param' property of request object.
+  // param is the property of request object that stores the route parameters in object form, in key value pairs.
+
+  // console.log(req.params);
+  // res.send(`test movie`);
+
+  // Now to find if the movie with the route parameter that we define 'id' do matches to the user's requested id or not.
+  // As the value of the route parameter's property is stored in string format we need to convert it into number format to work with it...
+  const newId = parseInt(req.params.id); //converted to number format
+  const movie = movieData.filter((el) => {
+    //checking if the particular id exists in our array of objects of movies using filter method.
+    if (el.id === newId) return el; // if it does exist, will simply return it.
+  });
+  // sometimes it may happen that user requests the id of the movies that doesn't exists in the datasource to handle such case we have.
+  // as filter returns the array of elements that passed the required condition we check if the array is empty so that we can send a fail message to the user with the specified not found status code.
+  if (movie.length === 0) {
+    return res.status(404).json({
+      status: "fail",
+      message: `Movie with id ${newId} not found`,
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      movie: movie,
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server started`);
